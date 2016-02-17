b22b58f2f501699ce39e04d6ad7fcabdebdc6ee3
Parth15495 <Parth15495@users.noreply.github.com>
2016-02-17 23:47:41 +0530

    Update Basic.cpp

--- Basic.cpp
+++ Basic.cpp
@@ -1,5 +1,7 @@
-#include<glut.h>
+#include<GL/glut.h>
 #include<cmath>
+#include<iostream>
+
 void init ()
 {
 	glClearColor(1,1,1,0);
@@ -25,19 +27,22 @@ void disp()
 				val=25;
 			if(val<-25)
 				val=-25;
-			*/	
+			*/
 			glVertex2f(50+i,150+val);
 			ctr+=0.03141592654;
 		}
 		glColor3f(0,0,1);
 			 ctr = 0.0;
+			 std::cout<<"Enter ltp (0 to-50) and upt (0 to +50) values bw 50 to -50 \n";
+			 double ltp,utp;
+			 std::cin>>ltp>>utp;
 		for(double i=0;i<200;i+=1)
 		{
 			double val =50*sinf(ctr);
-			if(val>25)
-				val=25;
-			if(val<-25)
-				val=-25;				
+			if(val>utp)
+				val=utp;
+			if(val<ltp)
+				val=ltp;
 			glVertex2f(50+i,150+val);
 			ctr+=0.03141592654;
 		}
@@ -47,9 +52,16 @@ void disp()
 			glVertex2i(50+i,150);
 		}*/
 	glEnd();
+	glColor3f(0.5,0.5,0);
+	char c[]="Schmitt Trigger Stimulation";
+	int i=0;
+	glRasterPos2i(40,30);
+	while(c[i]!='\0')
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,c[i++]);
 	glFlush();
+
 }
-void main(int argc,char **argv)
+int main(int argc,char **argv)
 {
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
